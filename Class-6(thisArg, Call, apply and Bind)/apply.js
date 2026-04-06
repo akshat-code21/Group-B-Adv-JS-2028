40fb294a3a95ae7b5a60984dbc80bb092d133808
Akshat <borncancer21@gmail.com>
2026-04-06 10:20:52 +0530

    push

--- Class-6(thisArg, Call, apply and Bind)/apply.js
+++ Class-6(thisArg, Call, apply and Bind)/apply.js
@@ -8,17 +8,14 @@
 
 //--XX--//
 
+let numbers = [10, 30, 40, 50];
 
-let numbers = [10 ,30 ,40 ,50]
+let max1 = Math.max(numbers); // why this does not work? => does not work because Math.max() does not accept an array — it expects individual numbers as arguments, not a single array.
 
- let max1 = Math.max(numbers) // why this does not work?
+let max2 = Math.max(...numbers);
 
-let max2 = Math.max(...numbers)
+let max3 = Math.max.apply(null, numbers); // here the arguements will be the array elements due to the nature of apply and thus does same job as spread operator.
 
-let max3 = Math.max.apply(null, numbers)
-
-
-
-console.log(max1)
-console.log(max2)
-console.log(max3)
+console.log(max1);
+console.log(max2);
+console.log(max3);
--- Class-6(thisArg, Call, apply and Bind)/call.js
+++ Class-6(thisArg, Call, apply and Bind)/call.js
@@ -1,6 +1,6 @@
-// function greet(city, country) {
-//   console.log("Hello " + this.name + " " + city + " " + country);
-// }
+function greet(city, country) {
+  console.log("Hello " + this.name + " " + city + " " + country);
+}
 
 const user = {
   name: "Adam",
@@ -15,5 +15,6 @@ const user2 = { name: "Steve" };
 
 // call
 
-// greet.call(user, "Bengaluru", "India");
-user.greet.call(user2, "Bengaluru", "India");
+greet.call(user, "Bengaluru", "India");
+user.greet.call(user2, "Bengaluru", "India"); // pass arguements without any additional bracket.
+// Also, here we borrowed the fn for user2 from user. The reference of the this keyword changed.
