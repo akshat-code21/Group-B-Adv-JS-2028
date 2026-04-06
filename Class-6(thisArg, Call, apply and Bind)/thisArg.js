dc14dbeb27cd14d85d9987d0bd2cd2d9f88f6449
Akshat <borncancer21@gmail.com>
2026-04-06 10:09:08 +0530

    push

--- Class-6(thisArg, Call, apply and Bind)/thisArg.js
+++ Class-6(thisArg, Call, apply and Bind)/thisArg.js
@@ -7,43 +7,48 @@
 
 // user.sayHi()
 
-
-
 //--xx--//
 
-
 const user = {
-    message: 'Hello'
-}
+  message: "Hello",
+};
 
-const names = ['Adam' , 'Bob' , 'Charles']
+const names = ["Adam", "Bob", "Charles"];
 
-
-names.forEach(function(name){
-   console.log(this.user , name)
-}, user)
-
+names.forEach(function (name) {
+  console.log(this.user, name);
+}, user);  // Here arrow fns also fail since it is not inside the object and thus takes global object as `this`
 
 //--XX--//
 
-
 const calculator = {
-    multiplier : 2,
+  multiplier: 2,
 
-    numbers : [1 ,2 ,3],
+  numbers: [1, 2, 3],
 
-    multiply(){
-        return this.numbers.map((num)=>{
-            
-           console.log(num)
-           return num * this.multiplier
-        })
-    }
-}
+  multiply() {
+    return this.numbers.map((num) => {
+      console.log(num);
+      return num * this.multiplier;
+    });
+  },
+};
 
-console.log(calculator.multiply())
+// console.log(calculator.multiply()); // arrow fns use lexical scoping and so they get reference of `this`. So we don't need to add thisArg.
 
+const calculatorNormal = {
+  multiplier: 2,
 
-// Call , Apply and Bind
+  numbers: [1, 2, 3],
 
+  multiply() {
+    return this.numbers.map(function (num) {
+      console.log(num);
+      return num * this.multiplier;
+    }, this);
+  },
+};
 
+console.log(calculatorNormal.multiply()); // returns undefined or NaN since it doesn't have reference to `this`. We need to pass it
+
+// Call , Apply and Bind. Used for thisArg / setting the this keyword.
