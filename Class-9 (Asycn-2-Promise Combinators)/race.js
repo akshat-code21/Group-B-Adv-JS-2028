4566f71109c701588dceb8f8c33c777519271f38
Akshat <borncancer21@gmail.com>
2026-04-08 13:43:01 +0530

    push

--- Class-9 (Asycn-2-Promise Combinators)/race.js
+++ Class-9 (Asycn-2-Promise Combinators)/race.js
@@ -42,35 +42,3 @@ Promise.any([
 
 // Write all the polyfills for all Combinators
 // COnstruct your Own Promise
-
-function all(promises) {
-  //write your code here ===============================================
-  return new Promise((resolve, reject) => {
-    if (!Array.isArray(promises)) {
-      return reject(new TypeError("Input must be an array"));
-    }
-
-    const results = [];
-    let completed = 0;
-
-    if (promises.length === 0) {
-      return resolve([]);
-    }
-
-    promises.forEach((promise, index) => {
-      // Handle non-promise values also
-      Promise.resolve(promise)
-        .then((value) => {
-          results[index] = value;
-          completed++;
-
-          if (completed === promises.length) {
-            resolve(results);
-          }
-        })
-        .catch(reject); // reject immediately
-    });
-  });
-}
-
-function myPromiseAll(promises) {}
