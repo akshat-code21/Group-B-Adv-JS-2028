f11abd565f719f9d40965fccf7231f1002ea7906
Akshat <borncancer21@gmail.com>
2026-04-12 15:55:16 +0530

    Refactor MyPromise class for improved readability and consistency; streamline promise resolution and rejection handling, and enhance error management in the then and catch methods.

--- practice-2/promise.js
+++ practice-2/promise.js
@@ -1,77 +1,76 @@
 class MyPromise {
-    constructor() {
-        this.state = "pending"
+  constructor() {
+    this.state = "pending";
 
-        this.value = undefined
-        this.reason = undefined
+    this.value = undefined;
+    this.reason = undefined;
 
-        this.onFulfilledCallbacks = []
-        this.onRejectedCallbacks = []
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
 
-        resolve = (value) => {
-            if (this.state === "pending") {
-                this.state = "fulfilled"
-                this.value = value
-                this.onFulfilledCallbacks.forEach(fn => fn(value))
-            }
-        }
+    resolve = (value) => {
+      if (this.state === "pending") {
+        this.state = "fulfilled";
+        this.value = value;
+        this.onFulfilledCallbacks.forEach((fn) => fn(value));
+      }
+    };
 
-        reject = (reason) => {
-            if (this.state === "pending") {
-                this.state = "rejected"
-                this.reason = reason
-                this.onRejectedCallbacks.forEach(fn => fn(reason))
-            }
-        }
+    reject = (reason) => {
+      if (this.state === "pending") {
+        this.state = "rejected";
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach((fn) => fn(reason));
+      }
+    };
 
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  then = (onFulfilled, onRejected) => {
+    return new Promise((res, rej) => {
+      const handleFulfilled = (value) => {
         try {
-            executor(resolve, reject);
+          if (typeof onFulfilled === "function") {
+            const result = onFulfilled(value);
+            res(result);
+          } else {
+            res(value);
+          }
         } catch (error) {
-            reject(error)
+          rej(error);
         }
-    }
+      };
 
-    then = (onFulfilled, onRejected) => {
-        return new Promise((res, rej) => {
-            const handleFulfilled = (value) => {
-                try {
+      const handleRejected = (reason) => {
+        try {
+          if (typeof onRejected === "function") {
+            const result = onRejected(reason);
+            res(result);
+          } else {
+            rej(reason);
+          }
+        } catch (error) {
+          rej(er);
+        }
+      };
 
-                    if (typeof onFulfilled === "function") {
-                        const result = onFulfilled(value);
-                        res(result)
-                    } else {
-                        res(value)
-                    }
-                } catch (error) {
-                    rej(error)
-                }
-            }
+      if (this.state === "fulfilled") {
+        setTimeout(() => handleFulfilled(this.value), 0);
+      } else if (this.state === "rejected") {
+        setTimeout(() => handleRejected(this.reason), 0);
+      } else {
+        this.onFulfilledCallbacks.push(handleFulfilled);
+        this.onRejectedCallbacks.push(handleRejected);
+      }
+    });
+  };
 
-            const handleRejected = (reason) => {
-                try {
-                    if (typeof onRejected === "function") {
-                        const result = onRejected(reason)
-                        res(result)
-                    } else {
-                        rej(reason)
-                    }
-                } catch (error) {
-                    rej(er)
-                }
-            }
-
-            if (this.state === "fulfilled") {
-                setTimeout(() => handleFulfilled(this.value, 0))
-            } else if (this.state === "rejected") {
-                setTimeout(() => handleRejected(this.reason, 0))
-            } else {
-                this.onFulfilledCallbacks.push(handleFulfilled)
-                this.onRejectedCallbacks.push(handleRejected)
-            }
-        })
-    }
-
-    catch = (onRejected) => {
-        return this.then(null, onRejected)
-    }
+  catch = (onRejected) => {
+    return this.then(null, onRejected);
+  };
 }
