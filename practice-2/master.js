837d61d28998420944b92bf1d541f99352694bd6
Akshat <borncancer21@gmail.com>
2026-04-12 11:56:49 +0530

    Refactor promise functions for consistency and readability; add custom array methods (myMap, myFilter, myReduce) with error handling

--- practice-2/master.js
+++ practice-2/master.js
@@ -1,138 +1,204 @@
 function myPromiseAll(items) {
-    return new Promise((res, rej) => {
-        if (!Array.isArray(items)) {
-            throw new TypeError("Items is not array")
-        }
+  return new Promise((res, rej) => {
+    if (!Array.isArray(items)) {
+      throw new TypeError("Items is not array");
+    }
 
-        if (items.length === 0) {
-            res([]);
-        }
+    if (items.length === 0) {
+      res([]);
+    }
 
-        let results = []
-        let completedCount = 0;
+    let results = [];
+    let completedCount = 0;
 
-        items.forEach((item) => {
-            Promise.resolve(item).then((data) => {
-                results.push(data)
-                completedCount++;
+    items.forEach((item) => {
+      Promise.resolve(item)
+        .then((data) => {
+          results.push(data);
+          completedCount++;
 
-                if (completedCount === items.length) {
-                    res(results)
-                }
-            }).catch((error) => {
-                rej(error)
-            })
+          if (completedCount === items.length) {
+            res(results);
+          }
         })
-    })
+        .catch((error) => {
+          rej(error);
+        });
+    });
+  });
 }
 
 function myPromiseAny(items) {
-    return new Promise((res, rej) => {
-        if (!Array.isArray(items)) {
-            return rej(new TypeError("Items is not an array"));
-        }
-        if (items.length === 0) {
-            res([]);
-        }
+  return new Promise((res, rej) => {
+    if (!Array.isArray(items)) {
+      return rej(new TypeError("Items is not an array"));
+    }
+    if (items.length === 0) {
+      res([]);
+    }
 
-        const errors = []
-        const errorCount = 0
+    const errors = [];
+    const errorCount = 0;
 
-        items.forEach((item) => {
-            Promise.resolve(item)
-                .then((data) => {
-                    res(data)
-                }).catch((error) => {
-                    errorCount++;
-                    errors.push(error)
-                    if (errorCount === array.length) {
-                        reject(new AggregateError(errors, 'All promises were rejected'));
-                    }
-                })
+    items.forEach((item) => {
+      Promise.resolve(item)
+        .then((data) => {
+          res(data);
         })
-    })
+        .catch((error) => {
+          errorCount++;
+          errors.push(error);
+          if (errorCount === array.length) {
+            reject(new AggregateError(errors, "All promises were rejected"));
+          }
+        });
+    });
+  });
 }
 
 function myPromiseRace(items) {
-    return new Promise((res, rej) => {
-        if (!Array.isArray(items)) {
-            return rej(new TypeError("Items is not an array"));
-        }
+  return new Promise((res, rej) => {
+    if (!Array.isArray(items)) {
+      return rej(new TypeError("Items is not an array"));
+    }
 
-        if (items.length === 0) {
-            return;
-        }
+    if (items.length === 0) {
+      return;
+    }
 
-        items.forEach((item) => {
-            Promise.resolve(item).then((value) => {
-                res(value)
-            }).catch((err) => {
-                rej(err)
-            })
+    items.forEach((item) => {
+      Promise.resolve(item)
+        .then((value) => {
+          res(value);
         })
-    })
+        .catch((err) => {
+          rej(err);
+        });
+    });
+  });
 }
 
 function myPromiseAllSettled(items) {
-    return new Promise((res, rej) => {
-        if (!Array.isArray(items)) {
-            return rej(new TypeError("Items is not an array"))
-        }
+  return new Promise((res, rej) => {
+    if (!Array.isArray(items)) {
+      return rej(new TypeError("Items is not an array"));
+    }
 
-        if (items.length === 0) {
-            res([])
-        }
+    if (items.length === 0) {
+      res([]);
+    }
 
-        const results = []
-        const completedCount = 0;
+    const results = [];
+    const completedCount = 0;
 
-        items.forEach((item) => {
-            Promise.resolve(item).then((value) => {
-                results.push({ status: "fulfilled", value: value })
-            }).catch((err) => {
-                results.push({ status: "rejected", reason: err })
-            }).finally(() => {
-                completedCount++;
-                if (completedCount === items.length) {
-                    resolve(results)
-                }
-            })
+    items.forEach((item) => {
+      Promise.resolve(item)
+        .then((value) => {
+          results.push({ status: "fulfilled", value: value });
         })
-    })
+        .catch((err) => {
+          results.push({ status: "rejected", reason: err });
+        })
+        .finally(() => {
+          completedCount++;
+          if (completedCount === items.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
 }
 
 Function.prototype.myCall = function (context, ...args) {
-    context = context | globalThis
+  context = context | globalThis;
 
-    const uniqueKey = Symbol("fn")
-    context[uniqueKey] = this
+  const uniqueKey = Symbol("fn");
+  context[uniqueKey] = this;
 
+  const result = context[uniqueKey](...args);
 
-    const result = context[uniqueKey](...args);
+  delete context[uniqueKey];
 
-    delete context[uniqueKey]
-
-    return result
-}
+  return result;
+};
 
 Function.prototype.myApply = function (ctx, argsArray) {
-    ctx = ctx || globalThis
-    const args = argsArray || []
+  ctx = ctx || globalThis;
+  const args = argsArray || [];
 
-    const uniqueKey = Symbol("fn")
-    ctx[uniqueKey] = this
+  const uniqueKey = Symbol("fn");
+  ctx[uniqueKey] = this;
 
-    const result = ctx[uniqueKey](...args)
+  const result = ctx[uniqueKey](...args);
 
-    delete ctx[uniqueKey]
+  delete ctx[uniqueKey];
 
-    return result
-}
+  return result;
+};
 
 Function.prototype.myBind = function (ctx, ...outerArgs) {
-    const originalFn = this
+  const originalFn = this;
 
-    return (...innerArgs) => {
-        return originalFn.apply(ctx, [...outerArgs, ...innerArgs])
+  return (...innerArgs) => {
+    return originalFn.apply(ctx, [...outerArgs, ...innerArgs]);
+  };
+};
+
+Array.prototype.myMap = function (callback, thisArg) {
+  if (typeof callback != "function") {
+    throw new TypeError("Cb not a function");
+  }
+
+  let arr = this;
+
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i in arr) {
+      res.push(cb.call(thisArg, arr[i]), i, arr);
     }
-}
+  }
+
+  return res;
+};
+
+Array.prototype.myFilter = function (callback, thisArg) {
+  if (typeof callback != "function") {
+    throw new TypeError("Cb not a function");
+  }
+
+  let arr = this;
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i in arr && callback(thisArg, arr[i], i, arr)) {
+      res.push(arr[i]);
+    }
+  }
+
+  return res;
+};
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  if (typeof callback != "function") {
+    throw new TypeError("Cb not a function");
+  }
+  if (this.length === 0 && arguments.length < 2) {
+    throw new TypeError("Array cannot be empty");
+  }
+  let i = 0;
+  let acc = initialValue;
+  let arr = this;
+
+  if (arguments.length < 2) {
+    acc = arr[0];
+    i = 1;
+  }
+
+  for (; i < arr.length; i++) {
+    if (i in this) {
+      acc = callback(acc, arr[i], i, this);
+    }
+  }
+  return acc;
+};
